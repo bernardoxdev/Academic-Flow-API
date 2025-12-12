2c1c3dc21cc9dfb8483bd7c42433e748d949f912
Bernardo de Castro <bernardocmfgomes@aluno.ufsj.edu.br>
2025-12-12 19:43:53 -0300

    Adicionada todas partes da API

---
+++ backend/api/atividades.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/atividades",
+    tags=["Atividades"]
+)
+
+if __name__ == '__main__':
+    pass
---
+++ backend/api/boca.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/boca",
+    tags=["BOCA"]
+)
+
+if __name__ == '__main__':
+    pass
