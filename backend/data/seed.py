47876827e5f3876bfb27e34aa8c85cb30cc10425
Bernardo de Castro <bernardocmfgomes@aluno.ufsj.edu.br>
2026-01-06 10:15:04 -0300

    fix: Arrumado get das variaveis do env

--- backend/data/seed.py
+++ backend/data/seed.py
@@ -14,12 +14,13 @@ except ImportError:
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
-ADMIN_ROLE = os.getenv("ADMIN_ROLE")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
 
 def criar_admin_se_nao_existir():
+    ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+    ADMIN_ROLE = os.getenv("ADMIN_ROLE")
+    ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+    
     db: Session = SessionLocal()
 
     admin = db.query(User).filter(User.username == ADMIN_USERNAME).first()
